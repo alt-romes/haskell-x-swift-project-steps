8126df8731caa28aa194e0a6dcb9941366500fe1
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-09 22:52:32 +0000

    Create XCode project

---
+++ SwiftHaskell/SwiftHaskell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftHaskell
+//
+//  Created by Rodrigo Mesquita on 09/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
