ac1ac239a8026b575dd4898a72e3e4d24149c051
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-09 22:52:59 +0000

    Call hs_double from ContentView

--- SwiftHaskell/SwiftHaskell/ContentView.swift
+++ SwiftHaskell/SwiftHaskell/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Haskell: \(hs_factorial(5))!")
         }
         .padding()
     }
