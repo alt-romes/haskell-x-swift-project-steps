bd39811a282092212f589b7c2023fc2ba65af29f
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-09 22:52:32 +0000

    Create XCode project

---
+++ SwiftHaskell/SwiftHaskell/SwiftHaskellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHaskellApp.swift
+//  SwiftHaskell
+//
+//  Created by Rodrigo Mesquita on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHaskellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
