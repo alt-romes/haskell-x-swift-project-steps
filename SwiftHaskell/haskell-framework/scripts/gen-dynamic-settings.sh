c23ad96b15b177faeef4816032a98b056a7a92bb
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-10 23:53:50 +0000

    Extend the library search path

--- SwiftHaskell/haskell-framework/scripts/gen-dynamic-settings.sh
+++ SwiftHaskell/haskell-framework/scripts/gen-dynamic-settings.sh
@@ -6,8 +6,14 @@ if ! test -f "haskell-framework/haskell-framework.cabal"; then
     exit 1
 fi
 
+pushd . > /dev/null
+cd haskell-framework
+FLIB_PATH=$(cabal list-bin haskell-foreign-framework)
+popd > /dev/null
+
 echo "
 HEADER_SEARCH_PATHS=\$(inherit) $(ghc-pkg field rts include-dirs --simple-output | tr ' ' '\n' | tail -n1)
+LIBRARY_SEARCH_PATHS=\$(inherit) $(dirname $FLIB_PATH)
 " > DynamicBuildSettings.xcconfig
 
 echo "Created DynamicBuildSettings.xcconfig!"
