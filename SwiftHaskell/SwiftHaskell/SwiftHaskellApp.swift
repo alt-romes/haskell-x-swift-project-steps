3e50680077957b388e2512b00510f62913076e75
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-11-11 00:36:49 +0000

    Call library init and deinit in main App

--- SwiftHaskell/SwiftHaskell/SwiftHaskellApp.swift
+++ SwiftHaskell/SwiftHaskell/SwiftHaskellApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import HaskellFramework.RTSManage
 
 @main
 struct SwiftHaskellApp: App {
+    init() {
+        flib_init()
+
+        NotificationCenter.default.addObserver(forName: NSApplication.willTerminateNotification, object: nil, queue: .main) { _ in
+            // terminating
+            flib_end()
+        }
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
