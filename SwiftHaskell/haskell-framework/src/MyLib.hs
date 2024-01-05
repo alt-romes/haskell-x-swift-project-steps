0c8cfd39553cd4e9d5b4edaad6e5120b1058b834
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-01-05 15:27:03 +0000

    Derive JSON instances for User

--- SwiftHaskell/haskell-framework/src/MyLib.hs
+++ SwiftHaskell/haskell-framework/src/MyLib.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE DerivingStrategies, DeriveAnyClass #-}
 module MyLib where
 
 import Foreign.C
+import Data.Aeson
+import GHC.Generics
 
 hs_factorial :: CInt -> CInt
 hs_factorial x = product [1..x]
@@ -9,3 +12,5 @@ data User = User
   { name :: String
   , age :: Int
   }
+  deriving stock Generic
+  deriving anyclass (ToJSON, FromJSON)
