83776f21c89cb38c7c231dbc6b9188f1e79f79b4
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-03-08 13:41:51 +0000

    Implement the Swift function which calls the Haskell-exported C foreign function

--- SwiftHaskell/SwiftHaskell/ContentView.swift
+++ SwiftHaskell/SwiftHaskell/ContentView.swift
@@ -8,20 +8,67 @@
 import SwiftUI
 import HaskellFramework.MyForeignLib_stub
 
-struct User {
+struct User: Codable {
     let name: String
     let age: Int
 }
 
 // birthday(user: User(name: "Anton", age: 33)) = User(name: "Anton", age: 34)
-func birthday(user: User) -> User {
-    // Calls Haskell function...
+func birthday (user : User) -> User {
+    let enc = JSONEncoder()
+    let dec = JSONDecoder()
+    do {
+        var data : Data = try enc.encode(user)
+        let data_len = Int64(data.count)
+        return try data.withUnsafeMutableBytes { (rawPtr:UnsafeMutableRawBufferPointer) in
+
+            // Allocate buffer for result
+            let buf_size = 1024000
+
+            return try withUnsafeTemporaryAllocation(of: Int.self, capacity: 1) { size_ptr in
+                size_ptr.baseAddress?.pointee = buf_size
+
+                do {
+                    return try withUnsafeTemporaryAllocation(byteCount: buf_size, alignment: 1) {
+ res_ptr in
+
+                        c_birthday(rawPtr.baseAddress, data_len, res_ptr.baseAddress, size_ptr.baseAddress)
+
+                        if let required_size = size_ptr.baseAddress?.pointee {
+                            if required_size > buf_size {
+                                throw HsFFIError.requiredSizeIs(required_size)
+                            }
+                        }
+                        return try dec.decode(User.self, from: Data(bytesNoCopy: res_ptr.baseAddress!, count: size_ptr.baseAddress?.pointee ?? 0, deallocator: .none))
+                    }
+                } catch HsFFIError.requiredSizeIs(let required_size) {
+                    print("Retrying with required size: \(required_size)")
+                    return try withUnsafeTemporaryAllocation(byteCount: required_size, alignment:
+ 1) { res_ptr in
+                        size_ptr.baseAddress?.pointee = required_size
+
+                        c_birthday(rawPtr.baseAddress, data_len, res_ptr.baseAddress, size_ptr.baseAddress)
+
+                        return try dec.decode(User.self, from: Data(bytesNoCopy: res_ptr.baseAddress!, count: size_ptr.baseAddress?.pointee ?? 0, deallocator: .none))
+                    }
+                }
+            }
+        }
+    } catch {
+        print("Error decoding JSON probably: \(error)")
+        return User(name: "", age: 0)
+    }
+}
+
+enum HsFFIError: Error {
+    case requiredSizeIs(Int)
 }
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, Haskell: \(hs_factorial(5))!")
+            let user = birthday(user: User(name: "Ellie", age: 24))
+            Text("Post-birthday, \(user.name) is: \(user.age)!")
         }
         .padding()
     }
