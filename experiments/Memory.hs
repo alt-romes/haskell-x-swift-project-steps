d1a15f42d4e09eedcce7b10c77586646c7e2e63a
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-03-09 11:49:50 +0000

    Add Haskell memory representation experiment

---
+++ experiments/Memory.hs
@@ -0,0 +1,13 @@
+module Memory where
+
+data Rect
+  = Rect { width :: Int
+         , height :: Int
+         }
+
+double :: Rect -> Rect
+double Rect{width=x, height=y} = Rect{width=x*2, height=y*2}
+
+myrect = Rect{width=12, height=24}
+
+area = putStrLn ("Area: " ++ show (width myrect * height myrect))
